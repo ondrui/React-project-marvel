99d58a10d5e3376439415eaa2259f69f494fe106
ondrui <ondrui@yandex.ru>
2021-11-19 01:24:51 +0300

    add new react-app marvel

---
+++ src/services/MarvelService.js
@@ -0,0 +1,36 @@
+class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=724f1ef6b7c682d8d0695a370b6c63a0';
+
+  getResourse = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllCharacters = async () => {
+    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+  }
+
+  getCharacter = async (id) => {
+    const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
+}
+
+export default MarvelService;
