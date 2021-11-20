d97b520d30876889969983776019b8b8d4e4d980
ondrui <ondrui@yandex.ru>
2021-11-21 02:06:00 +0300

    implemented logic of components App, ErrorBoundary, ErrorMessage, Spinner, RandomChar, CharList, CharInfo  and service request to API

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,113 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
 
-const CharList = () => {
+import './charList.scss';
+
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+    newItemLoading: false,
+    offset: 1557,
+    charEnded: false
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.onRequest();
+  }
+
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService
+        .getAllCharacters(offset)
+        .then(this.onCharListLoaded)
+        .catch(this.onError);
+  }
+
+  onCharListLoading = () => {
+    this.setState({
+      newItemLoading: true
+    });
+  }
+
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({offset, charList}) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended
+    }));
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  render() {
+    const { charList, loading, error, newItemLoading, offset, charEnded } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View charList={charList} props={this.props}/> : null;
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <div className='char__list'>
+        {errorMessage}
+        {spinner}
+        <ul className='char__grid'>{content}</ul>
+        <button
+          className='button button__main button__long'
+          onClick={() => this.onRequest(offset)}
+          disabled={newItemLoading}
+          style={{display: charEnded ? 'none' : 'block'}}
+        >
+          <div className='inner'>load more</div>
+        </button>
+      </div>
+    );
+  }
 }
+
+const View = ({ charList, props }) => {
+  return charList.map(({ name, thumbnail, id }) => {
+    // let imgStyle = { objectFit: 'cover' };
+    // if (thumbnail.indexOf('not_available') !== -1) {
+    //   imgStyle = { objectFit: 'fill' };
+    // }
+
+    const active = props.charId === id;
+    const clazz = active ? 'char__item char__item_selected' : 'char__item ';
+    return (
+      <li
+        key={id}
+        onClick={() => props.onCharSelected(id)}
+        className={clazz}>
+          <img
+            src={thumbnail}
+            //style={imgStyle}
+            style={
+              thumbnail.indexOf('not_available') !== -1
+                ? { objectFit: 'fill' }
+                : { objectFit: 'cover' }
+            }
+            alt={name}
+          />
+        <div className='char__name'>{name}</div>
+      </li>
+    );
+  });
+};
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=724f1ef6b7c682d8d0695a370b6c63a0';
+  _baseOffset = 210;
 
   getResourse = async (url) => {
     const res = await fetch(url);
@@ -12,8 +13,8 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = async () => {
-    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
   }
 
@@ -24,11 +25,13 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
-      wiki: char.urls[1].url
+      wiki: char.urls[1].url,
+      comics: char.comics.items.slice(0, 10)
     }
   }
 }
